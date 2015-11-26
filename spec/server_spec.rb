646a4bd5a45198a28abc5432b68039e5ca4b74df
Tobi Oduah <tobi.oduah@andela.com>
2015-11-26 14:57:34 +0100

    more tests

---
+++ spec/server_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe "Server" do
+  
+  let(:values_wrong) { {key: "asdlfa", full_url: "http://gmail.com", short_url: "gmailsite" } }
+  let(:values_right) { {key: "asdlfa", full_url: "http://gmail.com", short_url: "gmailsite" } }
+  
+  let(:subject_wrong) { Gobble::Tobi::ServerGateway.new(:values_wrong) }
+  let(:subject_right) { Gobble::Tobi::ServerGateway.new(:values_right) }
+  
+  describe "Server#initialize" do
+    it "initializes correctly" do
+      expect(subject_wrong).to be_a Gobble::Tobi::ServerGateway
+    end
+  end
+  
+end

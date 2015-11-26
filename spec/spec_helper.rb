01262f6f4fdc92d83a0f0b57d1877c55c19da1d6
Tobi Oduah <tobi.oduah@andela.com>
2015-11-26 15:21:13 +0100

    add codeclimate integration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
 require "gobble/version"
 require "gobble/tobi/parser"
 require "gobble/tobi/constants"
